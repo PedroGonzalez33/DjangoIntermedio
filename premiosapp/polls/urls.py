f573d0d4808a1639b14291eb09606dbc3ebf0ae0
Peter33 <pedro3gonzalez3@gmail.com>
2023-07-06 21:24:57 -0400

    Fin Basico Django

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.IndexView.as_view(), name="index"),
+
+    path("detail/<int:question_id>", views.DetailView.as_view(), name="detail"),
+
+    path("results/<int:question_id>", views.ResultView.as_view(), name="results"),
+
+    path("vote/<int:question_id>", views.vote, name="vote"),
+
 ]
